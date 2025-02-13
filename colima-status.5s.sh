fff45b5751a27b73a52aad132bc0c2e1e3303ea5
tonegawa07 <fukudayusuke07@gmail.com>
2025-02-14 00:25:10 +0900

    create colima-status

---
+++ colima-status.5s.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+COLIMA_PATH=$(which colima)
+
+STATUS=$($COLIMA_PATH status 2>&1)
+
+if echo "$STATUS" | grep -q "colima is running"; then
+    echo "🐳"
+    echo "---"
+    echo "Stop Colima | bash='$COLIMA_PATH stop' terminal=false refresh=true"
+    echo "Restart Colima | bash='$COLIMA_PATH restart' terminal=false refresh=true"
+else
+    exit 0
+fi
